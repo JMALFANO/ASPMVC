6f77dd61721aac659962cd58b6a3b66117a9e181
Juan Martin Alfano <juanmartinalfano@gmail.com>
2020-09-03 16:35:15 -0300

    + Clases (capaAccesoADatos) + Clases (capaNegocio)

---
+++ CapaAccesoADatos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoADatos
+{
+    public class Conexion
+    {
+        protected SqlConnection objConexion = new SqlConnection();
+        protected SqlCommand objComando = new SqlCommand();
+        protected string SQL;
+        public Conexion()
+        {
+            try
+            {
+                this.objConexion.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ASPNETMVC"].ConnectionString;
+                this.objComando.Connection = this.objConexion;
+                this.objComando.CommandType = System.Data.CommandType.Text;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
